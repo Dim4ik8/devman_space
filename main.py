785c0ed56db1c9064040ae4d69e388d34dc98c5b
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-08 19:50:49 +0300

    Download a picture

---
+++ main.py
@@ -0,0 +1,12 @@
+# https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg
+
+import requests
+
+filename = 'hubble.jpeg'
+url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as file:
+    file.write(response.content)
