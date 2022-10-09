7d742111913de6272e2a3deb07f159c0c9a88fd9
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-09 10:49:30 +0300

    Create def fetch_spacex_last_launch

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-# https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg
-
 import requests
+import os
 
-filename = 'hubble.jpeg'
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
-response = requests.get(url)
-response.raise_for_status()
+def save_picture(url, path):
+    directory = path
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    link = url
+    filename = 'hubble.jpeg'
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(f'{directory}/{filename}', 'wb') as file:
+        file.write(response.content)
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+
+def fetch_spacex_last_launch(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    photos = response.json()['links']['flickr']['original']
+    directory = path
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for number, photo in enumerate(photos):
+        with open(f'{directory}/spacex_{number}', 'wb') as file:
+            file.write(response.content)
+
+
+def main():
+    spaceX = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+
+    fetch_spacex_last_launch(spaceX, 'images')
+
+
+if __name__ == '__main__':
+    main()
