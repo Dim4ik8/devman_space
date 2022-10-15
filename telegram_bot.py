ca20251229f9265f31cebc1087d243ed089d2c20
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-15 13:00:13 +0300

    Publish photo to telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,4 +8,4 @@ token = os.getenv('TELEGRAM_TOKEN')
 bot = telegram.Bot(token=token)
 chat_id = os.getenv('CHANNEL_ID')
 
-bot.send_message(chat_id=chat_id, text="Привет! Вас рад видеть бот!! :))")
+bot.send_document(chat_id=chat_id, document=open('images/picture_hello.png', 'rb'))
