411a4362a419e77c85de31005dc4c53527205886
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-15 12:55:17 +0300

    Publish text to telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,5 +6,6 @@ load_dotenv()
 token = os.getenv('TELEGRAM_TOKEN')
 
 bot = telegram.Bot(token=token)
+chat_id = os.getenv('CHANNEL_ID')
 
-print(bot.get_me())
+bot.send_message(chat_id=chat_id, text="Привет! Вас рад видеть бот!! :))")
